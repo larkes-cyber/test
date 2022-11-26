94d557c5d0ccac75185827c6b9c639b83d623700
Your Name <playegrovoy@gmail.com>
2022-11-26 14:45:24 +0300

    nav setup

---
+++ app/src/main/java/com/example/travenor/presentation/screen/splash/SplashScreen.kt
@@ -0,0 +1,18 @@
+package com.example.travenor.presentation.screen.splash
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.travenor.presentation.theme.LightPrimaryBlue
+
+@Composable
+fun SplashScreen(){
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(LightPrimaryBlue)
+    ){
+
+    }
+}
